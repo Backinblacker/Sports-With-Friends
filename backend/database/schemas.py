b792ac50823e2fbd169a2e8cdd69abc45a405625
Backinblacker <coachblacker@gmail.com>
2023-04-25 14:17:18 -0600

    📦 NEW: Schemas created

--- backend/database/schemas.py
+++ backend/database/schemas.py
@@ -75,3 +75,53 @@ class TeamSchema(ma.Schema):
     
 team_schema = TeamSchema()
 teams_schema = TeamSchema(many=True)
+
+class FanSchema(ma.Schema):
+    user_id = fields.Integer(primary_key=True)
+    username = fields.String(required=True)
+    email = fields.String(required=True)
+    team_id = fields.Integer()
+    team = ma.Nested(TeamSchema, many=True)
+    class Meta:
+        fields = ("user_id", "username", "email", "team_id", "team")
+    
+    @post_load
+    def create_fan(self,data,**kwargs):
+        return Fan(**data)
+
+fan_schema = FanSchema()
+fans_schema = FanSchema(many=True)
+
+class EstablishmentSchema(ma.Schema):
+    establishment_id = fields.Integer(primary_key=True)
+    name = fields.String(required=True)
+    address = fields.String(required=True)
+    city = fields.String(required=True)
+    state = fields.String(required=True)
+    zip_code = fields.Integer(required=True)
+    website = fields.String()
+    team_id = fields.Integer()
+    team = ma.Nested(TeamSchema, many=True)
+    class Meta:
+        fields = ("establishment_id", "name", "address", "city", "state", "zip_code", "website", "team_id", "team")
+    
+    @post_load
+    def create_fan(self,data,**kwargs):
+        return Establishment(**data)
+
+establishment_schema = EstablishmentSchema()
+establishmentss_schema = EstablishmentSchema(many=True)
+
+class ReviewSchema(ma.Schema):
+    review_id = fields.Integer(primary_key=True)
+    text = fields.String(required=True)
+    rating = fields.String(required=True)
+    class Meta:
+        fields = ("review_id", "text", "rating")
+    
+    @post_load
+    def create_fan(self,data,**kwargs):
+        return Review(**data)
+    
+review_schema = ReviewSchema()
+reviews_schema = ReviewSchema(many=True)
